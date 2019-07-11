8c2e370714411a1267e1f2afa5a3da788833119e
japes930 <jpvillaflores9@gmail.com>
2019-07-11 11:31:07 -0400

    Added controller paths

--- src/main/java/HomeController.java
+++ src/main/java/HomeController.java
@@ -18,4 +18,56 @@ public class HomeController {
         return"/authors";
     }
 
+    @RequestMapping("/github1")
+    public String Github1(){
+        return"/github1";
+    }
+
+    @RequestMapping("/github2")
+    public String Github2(){
+        return"/github2";
+    }
+
+    @RequestMapping("/design")
+    public String Design(){
+        return"/design";
+    }
+
+    @RequestMapping("/algorithms")
+    public String Algorithms(){
+        return"/algorithms";
+    }
+
+    @RequestMapping("/corejava1")
+    public String CoreJava1(){
+        return"/corejava1";
+    }
+
+    @RequestMapping("/corejava2")
+    public String CoreJava2(){
+        return"/corejava2";
+    }
+
+    @RequestMapping("/ooj1")
+    public String ooj1(){
+        return"/ooj1";
+    }
+
+    @RequestMapping("/ooj2")
+    public String ooj2(){
+        return"/ooj2";
+    }
+
+    @RequestMapping("/html")
+    public String html(){
+        return"/html";
+    }
+
+    @RequestMapping("/bootstrap")
+    public String bootstrap(){
+        return"/bootstrap";
+    }
+
+
+
 }
