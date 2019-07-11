fdcd3930559655d90ee1df60152c7263fdaceec6
japes930 <jpvillaflores9@gmail.com>
2019-07-11 17:05:34 -0400

    Added cards to landing page

--- src/main/java/HomeController.java
+++ src/main/java/HomeController.java
@@ -2,70 +2,70 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class HomeController{
     @RequestMapping("/")
     public String Home(){
-        return "/index";
+        return "index";
     }
 
     @RequestMapping("/bootcamp")
     public String Bootcamp(){
-    return"/bootcamp";
+    return "bootcamp";
     }
 
     @RequestMapping("/authors")
     public String Authors(){
-        return"/authors";
+        return "authors";
     }
 
     @RequestMapping("/github1")
     public String Github1(){
-        return"/github1";
+        return "github1";
     }
 
     @RequestMapping("/github2")
     public String Github2(){
-        return"/github2";
+        return "github2";
     }
 
     @RequestMapping("/design")
     public String Design(){
-        return"/design";
+        return "design";
     }
 
     @RequestMapping("/algorithms")
     public String Algorithms(){
-        return"/algorithms";
+        return "algorithms";
     }
 
     @RequestMapping("/corejava1")
     public String CoreJava1(){
-        return"/corejava1";
+        return "corejava1";
     }
 
     @RequestMapping("/corejava2")
     public String CoreJava2(){
-        return"/corejava2";
+        return "corejava2";
     }
 
     @RequestMapping("/ooj1")
     public String ooj1(){
-        return"/ooj1";
+        return "ooj1";
     }
 
     @RequestMapping("/ooj2")
     public String ooj2(){
-        return"/ooj2";
+        return "ooj2";
     }
 
     @RequestMapping("/html")
     public String html(){
-        return"/html";
+        return "html";
     }
 
     @RequestMapping("/bootstrap")
     public String bootstrap(){
-        return"/bootstrap";
+        return "bootstrap";
     }
 
 
