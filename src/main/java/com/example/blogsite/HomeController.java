9e41f312366c0accf02606b23f6e5579a27a461b
syhan11 <sueyoung.6311@gmail.com>
2019-07-11 17:09:25 -0400

    gitHub1.html githubinfo.txt style_github.css homectrl changes

--- src/main/java/com/example/blogsite/HomeController.java
+++ src/main/java/com/example/blogsite/HomeController.java
@@ -1,71 +1,73 @@
+package com.example.blogsite;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
-    @RequestMapping("/")
-    public String Home(){
-        return "/index";
+public class HomeController{
+    @RequestMapping("/index")
+    public String homePage(){
+        return "index";
     }
 
     @RequestMapping("/bootcamp")
-    public String Bootcamp(){
-    return"/bootcamp";
+    public String bootCamp(){
+        return "bootcamp";
     }
 
     @RequestMapping("/authors")
-    public String Authors(){
-        return"/authors";
+    public String authors(){
+        return "authors";
     }
 
     @RequestMapping("/github1")
-    public String Github1(){
-        return"/github1";
+    public String gitHubPage1(){
+        return "github1";
     }
 
     @RequestMapping("/github2")
     public String Github2(){
-        return"/github2";
+        return "/github2";
     }
 
     @RequestMapping("/design")
     public String Design(){
-        return"/design";
+        return "/design";
     }
 
     @RequestMapping("/algorithms")
     public String Algorithms(){
-        return"/algorithms";
+        return "/algorithms";
     }
 
     @RequestMapping("/corejava1")
     public String CoreJava1(){
-        return"/corejava1";
+        return "/corejava1";
     }
 
     @RequestMapping("/corejava2")
     public String CoreJava2(){
-        return"/corejava2";
+        return "/corejava2";
     }
 
     @RequestMapping("/ooj1")
     public String ooj1(){
-        return"/ooj1";
+        return "/ooj1";
     }
 
     @RequestMapping("/ooj2")
     public String ooj2(){
-        return"/ooj2";
+        return "/ooj2";
     }
 
     @RequestMapping("/html")
     public String html(){
-        return"/html";
+        return "/html";
     }
 
     @RequestMapping("/bootstrap")
     public String bootstrap(){
-        return"/bootstrap";
+        return "/bootstrap";
     }
 
 
